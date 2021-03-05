4b940f82696881f72016f8fbdc3736b08f151f05
Shishirjaman <37430991+Shishirjaman@users.noreply.github.com>
2021-03-06 02:44:57 +0600

    Add files via upload

--- corona/urls.py
+++ corona/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('coronalab.urls'))
+    path('',include('coronalab.urls')),
+    
 ]
