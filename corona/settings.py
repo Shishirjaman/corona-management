584ab5e181b5b19857f118a91c422782ababc65b
Shishirjaman <37430991+Shishirjaman@users.noreply.github.com>
2021-03-06 03:45:03 +0600

    Add files via upload

--- corona/settings.py
+++ corona/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/image')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
